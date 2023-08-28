06802f85a74a95c38ebaf116df86aa6223d82dec
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-08-28 09:40:49 -0700

    completed 5 + 6. TileList and Tile are functional

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {
+        description.map((element,i) => {
+          return <p key={i} className="tile">{element}</p>
+        })
+      }
     </div>
   );
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ arr }) => {
   return (
     <div>
-      
+      {arr.map(({ name, ...description }, i) => {
+        return (
+          <Tile name={name} description={Object.values(description)} key={i} />
+        )
+      })}
     </div>
   );
 };
